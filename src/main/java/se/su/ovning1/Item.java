a52fb91519a75193766c17ac9062b3b5927439f2
linus <linus00552@gmail.com>
2025-03-28 12:53:14 +0100

    Linus first commit

--- src/main/java/se/su/ovning1/Item.java
+++ src/main/java/se/su/ovning1/Item.java
@@ -3,6 +3,9 @@ package se.su.ovning1;
 public class Item {
     private String name;
 
+    public Item(String name) {
+        this.name = name;
+    }
     public String getName() {
         return name;
     }
