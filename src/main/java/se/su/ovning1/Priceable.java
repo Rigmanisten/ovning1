07f80ed85db90f61f07ba1dfb486e38e74a955ee
Rigmanisten <einar.gurell@gmail.com>
2025-04-01 18:02:17 +0200

    "Recording" class och interfaces skapade

--- src/main/java/se/su/ovning1/Priceable.java
+++ src/main/java/se/su/ovning1/Priceable.java
@@ -2,5 +2,6 @@ package se.su.ovning1;
 
 public interface Priceable {
     public double getPrice();
-
+    public double getVAT();
+    public double getPriceWithVAT();
 }
--- src/main/java/se/su/ovning1/Recording.java
+++ src/main/java/se/su/ovning1/Recording.java
@@ -1,4 +1,58 @@
 package se.su.ovning1;
 
-public class Recording {
+public class Recording implements PriceableWithVAT25 {
+    private String artist;
+    private String type;
+    private int year;
+    private int condition;
+    private double price;
+
+    public Recording(String artist, String type, int year, int condition, double price) {
+        this.artist = artist;
+        this.type = type;
+        this.year = year;
+        this.condition = condition;
+        this.price = price;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    @Override //Override från Priceable
+    public double getPrice() {
+        return price;
+    }
+
+    public double getOriginalPrice() {
+        return price;
+    }
+
+    @Override //Override är för att göra det tydligt att metoden "toString" ersätts, och för potentiellt hjälpa med felhantering
+    public String toString() {
+        return artist + " - " + type + " (" + year + "), condition: " + condition + ", price: $" + price;
+    }
+
+    //Interfaces
+    @Override
+    public double getVAT() {
+        return price * 0.25;
+    }
+
+    @Override
+    public double getPriceWithVAT() {
+        return getPrice() + getVAT();
+    }
 }
