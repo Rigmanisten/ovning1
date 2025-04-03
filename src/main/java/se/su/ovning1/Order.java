33475389c54fbba056e5176eb59253e9094540e9
linus <linus00552@gmail.com>
2025-04-03 11:30:11 +0200

    Item first draft v3

--- src/main/java/se/su/ovning1/Order.java
+++ src/main/java/se/su/ovning1/Order.java
@@ -1,10 +1,46 @@
 package se.su.ovning1;
 
 public class Order {
+    private static long orderNumber = 1;
+    private long counter;
     Item[] Items;
 
     Order(Item[] Items) {
         this.Items = Items;
+        this.orderNumber = orderNumber;
+        orderNumber++;
+    }
+
+    public String getReceipt(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Receipt for order #").append(orderNumber).append("\n");
+        sb.append("------------\n");
+
+            for(Item item : items)
+                if(item instanceof LongPlay lp){
+
+                }else if(item instanceof CompactDisc cp){
+
+                }else { // Book
+
+                }
+            return receipt;
+
+        }
+    public double getTotalValue (){
+        double total = 0;
+        for(Item item : items){
+            total += item.getPrice();  // måste var implemnterad
+        }
+        return total;
+    }
+
+    public double getTotalValuePlusVAT (){
+        double total = 0;
+        for(Item item : items){
+            total += item.getPriceWithVAT(); // måste var implemnterad
+        }
+        return total;
     }
 
 }
