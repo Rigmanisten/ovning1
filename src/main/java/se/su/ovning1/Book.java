2cc39719aa7cf12e80ae2deaab444cb595c2682d
linus <linus00552@gmail.com>
2025-04-04 15:42:53 +0200

    Book draft v1

--- src/main/java/se/su/ovning1/Book.java
+++ src/main/java/se/su/ovning1/Book.java
@@ -1,5 +1,43 @@
 package se.su.ovning1;
 
-public class Book {
+public class Book extends Item implements PriceableWithVAT6 {
+    private String author;
+    private boolean bound;
+    private double price;
 
+    public Book(String name, String author, double price, boolean bound) {
+        super(name);
+        this.author = author;
+        this.price = price;
+        this.bound = bound;
+    }
+
+
+    public String getType() {
+        return "Book";
+    }
+
+    public double getPrice() {
+        if (bound) {
+            return price * 1.3;
+        } else {
+            return price;
+        }
+    }
+
+    public double getPriceWithVAT() {
+        return this.getPrice() + this.getPrice() * getVAT();
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public boolean getBound() {
+        return this.bound;
+    }
+
+    public String toString() {
+        return ;
+    }
 }
