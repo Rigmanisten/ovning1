99b2ef0de4b9dcdc8e9cb612b695afde61ecf4d3
linus <linus00552@gmail.com>
2025-04-04 16:38:14 +0200

    Order first draft

--- src/main/java/se/su/ovning1/Order.java
+++ src/main/java/se/su/ovning1/Order.java
@@ -12,24 +12,30 @@ public class Order {
     }
 
     public String getReceipt(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("Receipt for order #").append(orderNumber).append("\n");
-        sb.append("------------\n");
-
-            for(Item item : items)
-                if(item instanceof LongPlay lp){
-
-                }else if(item instanceof CompactDisc cp){
-
-                }else { // Book
-
-                }
-            return sb.toString();
+        String receipt ="";
+        for(Item item : Items)
+            if(item instanceof LongPlay lp){
+                receipt = "Receipt for order #"+this.orderNumber+
+                        "/n-----------/n"+lp.getType()+
+                        " "+lp.toString()+ "/n Total excl. VAT: "+getTotalValue ()+
+                        "/n Total incl. VAT: "+getTotalValuePlusVAT();
+            }else if(item instanceof CompactDisc cp){
+                receipt = "Receipt for order #"+this.orderNumber+
+                        "/n-----------/n"+cp.getType()+
+                        " "+cp.toString()+ "/n Total excl. VAT: "+getTotalValue ()+
+                        "/n Total incl. VAT: "+getTotalValuePlusVAT();
+            }else if(item instanceof Book book){ // Book
+                receipt = "Receipt for order #"+this.orderNumber+
+                        "/n-----------/n"+book.getType()+
+                        " "+book.toString()+ "/n Total excl. VAT: "+getTotalValue ()+
+                        "/n Total incl. VAT: "+getTotalValuePlusVAT();
+            }
+        return receipt;
 
         }
     public double getTotalValue (){
         double total = 0;
-        for(Item item : items){
+        for(Item item : Items){
             total += item.getPrice();  // måste var implemnterad
         }
         return total;
@@ -37,7 +43,7 @@ public class Order {
 
     public double getTotalValuePlusVAT (){
         double total = 0;
-        for(Item item : items){
+        for(Item item : Items){
             total += item.getPriceWithVAT(); // måste var implemnterad
         }
         return total;
